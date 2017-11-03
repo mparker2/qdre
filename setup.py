cc86860a4691c6ee15baaea6aa0184606686eb55
Matthew Parker <mparker2@sheffield.ac.uk>
2017-11-03 13:24:47 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qdre',
+    version='0.1',
+    description='kill sge jobs with regex patterns',
+    author='matthew parker',
+    entry_points={
+        'console_scripts': ['qdre = qdre:qdre']
+    },
+    packages=[],
+    install_requires=['pandas', 'beautifulsoup4']
+)
